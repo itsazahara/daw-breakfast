8b341924081281dcaa89a6e98a6f9060b1350eb4
itsazahara <aromsch1403@g.educaand.es>
2025-02-03 13:56:36 +0100

    Subida del CrudRepository de review

--- src/main/java/com/daw/persistence/repository/ReviewRepository.java
+++ src/main/java/com/daw/persistence/repository/ReviewRepository.java
@@ -1,10 +1,19 @@
 package com.daw.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.ListCrudRepository;
 
+import com.daw.persistence.entities.Desayuno;
 import com.daw.persistence.entities.Review;
 
 public interface ReviewRepository extends ListCrudRepository<Review, Integer>{
+	
+	List<Review> findAllByOrderByFechaCreacionDesc();
+	List<Review> findAllByOrderByFechaCreacionAsc();
+	List<Review> findAllByOrderByPuntuacionDesc();
+	List<Review> findByDesayunoOrderByFechaCreacionDesc(Desayuno desayuno);
+	List<Review> findByDesayunoOrderByPuntuacionDesc(Desayuno desayuno);
 	
 	//NO TERMINADO
 
