d731a9cc2e4a318a06e6e36ab5e32acc1f654bfb
Humanito04 <pablohumanesgandul04@gmail.com>
2025-02-07 20:19:37 +0100

    Subida de cambios en las clases de Desayuno y prueba de los endpoints

--- src/main/java/com/daw/service/DesayunoService.java
+++ src/main/java/com/daw/service/DesayunoService.java
@@ -1,7 +1,9 @@
 package com.daw.service;
 
 import com.daw.persistence.entities.Desayuno;
+import com.daw.persistence.entities.Establecimiento;
 import com.daw.persistence.repository.DesayunoRepository;
+import com.daw.persistence.repository.EstablecimientoRepository;
 import com.daw.service.dtos.DesayunoDTO;
 import com.daw.service.mappers.DesayunoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,9 @@ public class DesayunoService {
     @Autowired
     private DesayunoMapper desayunoMapper;
 
+    @Autowired
+    private EstablecimientoRepository establecimientoRepository;
+
 
     public List<DesayunoDTO> listAll(){
         return this.desayunoRepository.findAll().stream().map(desayunoMapper::toDTO).collect(Collectors.toList());
@@ -33,16 +38,29 @@ public class DesayunoService {
         return this.desayunoRepository.existsById(idDesayuno);
     }
 
-    public Desayuno create(Desayuno desayuno){
-        desayuno.setPuntuacion(0.0);
-        if(desayuno.getImagen() == null){
+    public Desayuno create(Desayuno desayuno) {
+
+        if (desayuno.getImagen() == null || desayuno.getPuntuacion() == null) {
+            desayuno.setPuntuacion(0.0);
             desayuno.setImagen("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuGbZQa0K6RcMzEEnIcozJKd6Cu8wGBk8ThA&s");
         }
 
+        if (desayuno.getEstablecimiento() != null && desayuno.getEstablecimiento().getId() != null) {
+            Establecimiento establecimiento = establecimientoRepository
+                    .findById(desayuno.getEstablecimiento().getId())
+                    .orElseThrow(() -> new IllegalArgumentException("El establecimiento con ID " + desayuno.getEstablecimiento().getId() + " no existe."));
+
+            desayuno.setEstablecimiento(establecimiento);
+
+
+        }
         return this.desayunoRepository.save(desayuno);
     }
 
     public Desayuno update(Desayuno desayuno){
+        if(desayuno.getId() == null){
+            throw new IllegalArgumentException("El desayuno con ID " + desayuno.getId() + " no existe");
+        }
         return this.desayunoRepository.save(desayuno);
     }
 
